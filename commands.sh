6556e537b32d8b8f0045203f116287645308d326
Kåre von Geijer <kare.kvg@gmail.com>
2024-06-14 17:35:39 +0200

    Add more commands and comments

--- commands.sh
+++ commands.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Compares the two heuristics over differing operation and prefill counts. Tinker with script for nice axes
 python3 dual-pre-ops-heatmap.py -p 16 -i 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 -o 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 -r 200
+
+# See how the heuristic scales with p
+python partial-scaling.py -p 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 -o 10000 -i 100000 -r 10
