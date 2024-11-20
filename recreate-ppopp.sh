b15d7e1ff8f6dfd1724ed6a897e0e2b66bdc4779
Kåre von Geijer <kare.kvg@gmail.com>
2024-11-20 17:29:23 +0100

    Improve recreate.ppopp

--- recreate-ppopp.sh
+++ recreate-ppopp.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-python3 pre-ops-heatmap.py -r 1 -s 64 -o 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 -i 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 --title "d-RA"  --heuristic length    --save_path d-RA
-python3 pre-ops-heatmap.py -r 1 -s 64 -o 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 -i 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 --title "d-CBO" --heuristic operation --save_path d-CBO
+# The paper used runs=100, but we can see the pattern even with this smaller number. Increasing it might increase runtime (mitigated by parallelism)
+runs=1
+
+python3 pre-ops-heatmap.py -r $runs -s 64 -o 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 -i 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 --title "d-RA"  --heuristic length    --save_path d-RA
+python3 pre-ops-heatmap.py -r $runs -s 64 -o 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 -i 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 --title "d-CBO" --heuristic operation --save_path d-CBO
+echo "See output in 'd-RA.pdf' and 'd-CBO.pdf'"
